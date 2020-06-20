eb2e43d1c14d6480cca5a93c026615eba223d43e
rot <rubiobr@tutamail.com>
2020-06-20 23:25:12 +0100

    Spawn-Points

---
+++ Assets/Scripts/camara_Mov.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camara_Mov : MonoBehaviour
+{
+    public Transform playerPos;
+    public float x, y, z;
+
+
+    private void FixedUpdate() {
+
+        if (playerPos != null)
+        {
+            transform.position = new Vector3(playerPos.position.x, transform.position.y, playerPos.position.z - z);
+        }
+    }
+}
---
+++ Assets/SpawnEnemies.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour
+{
+    public GameObject enemy;
+
+    public float reSpawnTime = 0, waitTime = 10;
+    public bool canSpawn = false;
+
+    // Time to Spawn
+
+    //// Start is called before the first frame update
+    void Start()
+    {
+        //Cargo mi prefav aqui
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canSpawn)
+        {
+            SpawnEnemy();
+            canSpawn = false;
+        } else
+        {
+            reSpawnTime += Time.deltaTime;
+            if(reSpawnTime >= waitTime)
+            {
+                canSpawn = true;
+                reSpawnTime = 0;
+            }
+        }
+        //si puedo espan
+        // inicio my enemigo en la posicion de este spanw point
+
+    }
+    void SpawnEnemy() {
+        GameObject a = Instantiate(enemy, transform.position, Quaternion.identity);
+    }
+}
